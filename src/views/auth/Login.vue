d32e20e55d730a3c2ad92b999d8dd3a42f92476e
Daniel Mabadeje <mabadejedaniel1@gmail.com>
2025-08-11 16:36:54 +0100

    edited login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -82,7 +82,7 @@
         </div>
 
         <form @submit.prevent="handleLogin">
-          <select v-model="selectedRole">
+          <select v-model="selectedRole" class="w-full h-12 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
             <!-- <option value="general">General</option> -->
             <option value="medical_provider_user">Medical Provider User</option>
             <option value="doctor">Doctor</option>
