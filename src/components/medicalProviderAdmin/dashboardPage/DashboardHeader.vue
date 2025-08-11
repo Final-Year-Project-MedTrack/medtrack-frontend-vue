e873bf0446fd5b7f9303837aba175cb3ac1c884e
Daniel Mabadeje <mabadejedaniel1@gmail.com>
2025-08-11 16:17:48 +0100

    edited dashboard header

--- src/components/medicalProviderAdmin/dashboardPage/DashboardHeader.vue
+++ src/components/medicalProviderAdmin/dashboardPage/DashboardHeader.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="bg-green-50 p-4 w-full rounded-xl flex flex-col md:flex-row justify-between items-start md:items-center gap-2">
+  <div class="bg-green-50 p-4 w-full rounded-xl flex md:flex-row justify-between items-start md:items-center gap-2">
     <div class="w-1/2">
       <h1 v-if="userStore.user" class="text-lg font-semibold">Welcome, {{ userStore.user.first_name +' '+ userStore.user.last_name }}</h1>
       <h1 v-else>Guest</h1>
       <p class="text-sm text-gray-600">Medical Assistant</p>
     </div>
-    <div class="flex gap-2 flex-wrap w-1/2">
-      <button class="btn">Register patient</button>
-      <button class="btn">Record vitals</button>
-      <button class="btn btn-green">New visit</button>
+    <div class="flex gap-2 flex-wrap text-md font-thin">
+      <button class="btn bg-white p-2 px-3 m-2 border border-gray-200 rounded-lg">
+        <i class="fa fa-user-plus"></i>
+        Register patient
+      </button>
+      <button class="btn bg-white p-2 px-3 m-2 border border-gray-200 rounded-lg">
+        <i class="fa fa-regular fa-heart"></i>
+        Record vitals
+      </button>
+      <button class="btn bg-green-500 p-2 px-3 m-2 border border-gray-200 rounded-lg text-white">
+        <i class="fa fa-bookmark"></i>
+        New visit
+      </button>
     </div>
   </div>
 </template>
